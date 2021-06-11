08d55afa5fd54c74c2f2496a30416ca1b0e0e70b
tomasz osika <0kxait@gmail.com>
2021-06-11 13:08:48 +0200

    klasa gui, glowny program

--- main.cpp
+++ main.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
-#include "AccountList.h"
-#include "BankManager.h"
-#include "Data/AccountMock.h"
-#include "Data/LedgerMock.h"
+#include "src/BankManagerLogger.h"
+#include "src/Data/AccountMock.h"
+#include "src/Data/LedgerMock.h"
+#include "src/Log/FileLogger.h"
+#include "src/Log/ConsoleLogger.h"
+#include "src/Log/UserLogger.h"
+#include "src/GUI/BankManagerGui.h"
+#include "src/DALC/ConfigDALC.h"
+#include "src/Data/ConfigMock.h"
+#include "src/Data/ConfigCsv.h"
+#include "src/Data/LedgerCsv.h"
+#include "src/Data/AccountCsv.h"
+#include "src/BankConfig.h"
 
 int main() {
-    auto ledg = dynamic_cast<LedgerDALC*>(new LedgerMock());
-    auto acc = dynamic_cast<AccountDALC*>(new AccountMock());
+    auto conf = dynamic_cast<ConfigDALC*>(new ConfigCsv("config.csv"));
 
-    auto manager = new BankManager(ledg, acc);
+    BankConfig* bc;
 
     try {
-        manager->createTransaction(1, 2, 575757575757);
-    }catch(const std::invalid_argument& e) {
-        std::cout << "COS SIE ZEPSULO!!!:" << std::endl;
-        std::cout << e.what() << std::endl;
+        bc = new BankConfig(conf);
+    }catch(const std::exception& e) {
+        std::cout << "nie udalo sie otworzyc pliku konfiguracyjnego. upewnij sie ze w folderze z plikiem wykonywalnym znajduje sie plik konfiguracyjny config.csv. plik konfiguracyjny jest plikiem csv rozdzielanym znakiem ';'" << std::endl;
+        std::cout << "dodatkowe informacje: " << e.what() << std::endl;
+        exit(-1);
     }
 
-    auto a = manager->getAccountList();
-    for(auto i : a) {
-        std::cout << i.Id() << ", " << i.Name() << ", " << i.Pesel() << ", " << i.Balance() << std::endl;
-    }
+    auto ledgDbLoc = bc->LedgerDbLocation();
+    auto accDbLoc = bc->AccountsDbLocation();
+
+    auto ledg = dynamic_cast<LedgerDALC*>(new LedgerCsv(ledgDbLoc));
+    auto acc = dynamic_cast<AccountDALC*>(new AccountCsv(accDbLoc));
+
+    UserLogger l{};
+
+    FileLogger fl{"log.txt"};
+    ConsoleLogger cl;
+
+    l.registerLogger(dynamic_cast<Logger*>(&fl));
+    //l.registerLogger(dynamic_cast<Logger*>(&cl));
+
+    auto manager = new BankManagerLogger(ledg, acc, &l);
 
     manager->saveData();
+
+    BankManagerGui a{dynamic_cast<BankManager*>(manager)};
+    a.MakeMenu();
+
     return 0;
 }
